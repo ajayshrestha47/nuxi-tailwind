95543ad58bee373d64574bab709cb5779a06f054
AJAY SHRESTHA <shrestha.azay@gmail.com>
2023-06-16 22:43:56 +0545

    Pages Setup

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-4xl text-center mt-10">
-      Hello <br />
-      <Icon name="mdi:nuxt" color="black" /> Nuxt3
-      <Icon name="mdi:tailwind" color="black" /> Tailwind
-    </h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-<script setup lang="ts">
-</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-center mt-10">
+      Hello <br />
+      <Icon name="mdi:nuxt" color="black" /> Nuxt3
+      <Icon name="mdi:tailwind" color="black" /> Tailwind
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
